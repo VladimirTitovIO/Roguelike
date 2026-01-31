1a9c4efe725b460c4e30b9704a936e758db5c7f0
Yolanmar <anna.plahova@gmail.com>
2026-01-31 15:23:38 +0300

    presentation

---
+++ src/AP1_Jv_P01/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    id ("application")
+}
+
+group = "presentation"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.googlecode.lanterna:lanterna:3.1.3") // добавлено
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass.set("presentation.Main")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
